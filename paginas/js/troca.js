79faabe3f7e570560a5330232da9718d16b6b5c4
Jonatasilva05 <jonatasmoraes05@gmail.com>
2023-09-03 00:26:19 -0300

    -----

--- paginas/js/troca.js
+++ paginas/js/troca.js
@@ -1,6 +1,6 @@
-const computador = document.querySelector(".content");
-const cell = document.querySelector('.content-comp');
-const mediaQuery = window.matchMedia("(max-width: 1100px)");
+let computador = document.querySelector(".content");
+let cell = document.querySelector('.content-comp');
+let mediaQuery = window.matchMedia("(max-width: 1100px)");
 
 function esconderElemento(mediaQuery) {
   if (mediaQuery.matches) {
@@ -14,3 +14,33 @@ function esconderElemento(mediaQuery) {
 
 esconderElemento(mediaQuery);
 mediaQuery.addListener(esconderElemento);
+
+
+
+// const computador = document.querySelector(".content");
+// // const cell = document.querySelector('.content-comp');
+// const mediaQuery = window.matchMedia("(max-width: 1100px)");
+// var divsComp = document.getElementsByClassName ("content-comp");
+
+// function esconderElemento(mediaQuery) {
+//   if (mediaQuery.matches) {
+//     computador.style.display = "none";
+//     cell.style.display = "none";
+//   } else {
+//     computador.style.display = "block";
+//     cell.style.display = "none";
+//   }
+// }
+
+// var divsComp = document.getElementsByClassName ("desabilitar");
+//   for (var i = 0; i < divs.length; i++)
+// var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+//   var divs = document.getElementsByClassName ("desabilitar");
+//   for (var i = 0; i < divs.length; i++) {
+//     if (w < 375) {
+//       divs [i].style.display = "none";
+//     }
+//     else {
+//       divs [i].style.display = "block";
+//     }
+//   }
