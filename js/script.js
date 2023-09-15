6430a7ee357f3123a4f88912b7c18a6bddae14ee
Jonatasilva05 <jonatasmoraes05@gmail.com>
2023-09-15 01:50:00 -0300

    -----

--- js/script.js
+++ js/script.js
@@ -1,48 +1,29 @@
-/* Ação de eventos sobre o botão menu da tag nav (mobile). */
-/* Parâmetros: Nome do evento, função. */
-openMenu.addEventListener('click', () => {
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
 
-	/* Sobrepõe a propriedade display: none aplicada no
-	   CSS por display: flex que o torna visível. */ 
-	menu.style.display = "flex"
+document.querySelectorAll('.products-container .product').forEach(product =>{
+  product.onclick = () =>{
+    preveiwContainer.style.display = 'flex';
+    let name = product.getAttribute('data-name');
+    previewBox.forEach(preview =>{
+      let target = preview.getAttribute('data-target');
+      if(name == target){
+        preview.classList.add('active');
+      }
+    });
+  };
+});
 
-	/* Captura o tamanho do menu nav e aplica na posição. */
-	menu.style.right = (menu.offsetWidth * -1) + 'px'
+previewBox.forEach(close =>{
+  close.querySelector('.btnFechar').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
 
-	/* Após 10 milésimos de segundo, adiciona o atributo style, */
-	/* e adiciona as propriedades CSS.*/
-	setTimeout(()=> {
-		/* Faz o menu nav aparecer na velocidade em que foi
-		   determinado na propriedade transition no CSS.*/
-		menu.style.opacity = '1'
-
-		/* Move o menu nav para a posição 0 a direita. Utiliza 
-		   também a velocidade definida, na propriedade transition 
-		   no CSS para realizar o movimento mais suave.*/
-		menu.style.right = "0"
-
-		/* Oculta o botão que torna visível o elemento nav.*/
-		openMenu.style.display = 'none'
-	}, 10);
-})
-
-/* Ação de eventos sobre o botão X da tag nav (mobile). */
-/* Parâmetros: Nome do evento, função. */
-closeMenu.addEventListener('click', () => {
-
-	/* Faz o menu nav desaparecer na velocidade em que foi
-	   determinado na propriedade transition no CSS. */
-	menu.style.opacity = '0'
-
-	/* Captura o tamanho do menu nav e aplica na posição. */
-	menu.style.right = (menu.offsetWidth * -1) + 'px'
-
-	/* Torna visível o botão que apresenta o menu nav. */
-	/* openMenu.style.display = 'block'*/
-	
-	/* Após 200 milésimos de 1 segundo, remove o atributo style. */
-	setTimeout(()=> {
-		menu.removeAttribute('style')
-		openMenu.removeAttribute('style')
-	}, 200);
-})
+previewBox.forEach(close =>{
+  close.querySelector('.fas.fa-times').onclick = () =>{
+    close.classList.remove('active');
+    preveiwContainer.style.display = 'none';
+  };
+});
