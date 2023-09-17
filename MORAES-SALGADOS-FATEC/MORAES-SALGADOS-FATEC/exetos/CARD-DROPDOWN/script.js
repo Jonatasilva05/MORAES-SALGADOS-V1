fd28ec942e5a1d03602cc05d2f0b5a2344134f1c
Jonatasilva05 <jonatasmoraes05@gmail.com>
2023-09-17 01:24:44 -0300

    -----

--- MORAES-SALGADOS-FATEC/MORAES-SALGADOS-FATEC/exetos/CARD-DROPDOWN/script.js
+++
@@ -1,6 +0,0 @@
-const card = document.querySelector(".card");
-const cardtoggle = document.querySelector(".toggle");
-
-cardtoggle.onclick = function(){
-    card.classList.toggle("active");
-};
--- MORAES-SALGADOS-FATEC/MORAES-SALGADOS-FATEC/exetos/prontosub/script copy.js
+++
@@ -1,6 +0,0 @@
-const content = document.querySelector(".content");
-const cardtoggle = document.querySelector(".toggle");
-
-cardtoggle.onclick = function(){
-    content.classList.toggle("active");
-};
