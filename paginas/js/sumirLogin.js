ae5536d5ab782fd4be4c778621ec15f72cadc896
Jonatasilva05 <jonatasmoraes05@gmail.com>
2023-09-17 15:45:14 -0300

    -----

---
+++ paginas/js/sumirLogin.js
@@ -0,0 +1,25 @@
+const te = document.querySelector('.login-box');
+const Ativador = document.querySelector('#openMenu');
+const removedor = document.querySelector('#closeMenu');
+
+Ativador.addEventListener('click', () => {
+    te.classList.add('sumiu');
+})
+
+removedor.addEventListener('click', () => {
+    te.classList.remove('sumiu');
+})
+
+
+
+// window.addEventListener("resize", function() {
+//     if (window.innerWidth < 1100) {
+//         Ativador.addEventListener('click', () => {
+//             main.classList.add('active');
+//         })
+//         } 
+//         // else
+//         // {
+//         //     main.classList.remove('active');
+//         // }
+// });
