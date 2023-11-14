5edc204e12aa69ff3d69ec063d66b6213d72911e
Jonatasilva05 <jonatasmoraes05@gmail.com>
2023-11-14 14:34:19 -0300

    MORAES-SALGADOS novo dps do error

---
+++ testes/teste.js
@@ -0,0 +1,22 @@
+
+
+//esse é o jeito de trocar o local
+// var title = document.querySelector('#teste');
+
+// console.log(title);
+
+// title.textContent = "teste de troca"
+
+
+
+//escondendo div com click
+// var btn = document.querySelector("#esconder");
+// var cont = document.querySelector(".ola");
+
+// btn.addEventListener('click', function(){
+//     if(cont.style.display === 'block') {
+//         cont.style.display = 'none';
+//     } else {
+//         cont.style.display = 'block';
+//     }
+// })
