333278b8536ca1216ed3dd916711de2fdaaf6e9d
Jonatasilva05 <jonatasmoraes05@gmail.com>
2023-11-16 17:19:31 -0300

    -----

--- JS/index.js
+++ JS/index.js
@@ -12,12 +12,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // Cria a caixa de mensagem
     var messagebox = document.createElement('div');
     messagebox.classList.add('message-box');
-    messagebox.innerHTML = '<form>   <div class="profileForm">   <h1 class="gradient-text">BEM-VINDO</h1>   <div class="linhaForm1"></div>   <div class="linhaForm2"></div>   <p><span>Novo por aqui?</span><br>Deseja fazer seu primeiro cadastro!?</p>   <button class="btn">Cadastrar-me!</button>   <br><br>   <button class="btn" onclick="fecharMensagem()">Não Quero Agora</button>   <div class="botnProfi">   <label class="lblProfile">Lembre-me mais tarde</label>   <input type="checkbox" id="bellCheckbox">   <label for="bellCheckbox" class="bellLabel">   </label>   </div>   </div>   </form>';
+    messagebox.innerHTML = '<form>  <h2>Bem-vindo à nossa página!</h2> <p>Deseja fazer seu primeiro cadastro!?</p>  <label>Digite seu Nome:<label>  <input type="text" placeholder="Digite seu Nome: ">  <br> <label>Digite seu Sobrenome</label>  <input type="text" placeholder="Digite seu sobrenome: ">  <br>   <input type="date">  <button onclick="fecharMensagem()">Fechar</button>  </form>';
   
     // Adiciona a caixa de mensagem ao corpo do documento
     document.body.appendChild(messagebox);
   }
-
   
   // Função para fechar a mensagem
   function fecharMensagem() {
